e6e0a8f1336664a3b3cba842d12d9dafaa9617c6
Tharwin <tharwin.carr08@gmail.com>
2021-07-01 19:38:13 -0700

    TC: initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+	return (
+		<footer className='info'>
+			<p>Double-click to edit a todo</p>
+			<p>Click 'Urgent!' to make task a priority</p>
+			<p>
+				Created by <a href='http://github.com/jacob-ebey/'>jacob-ebey</a>
+			</p>
+			<p>
+				Updated by <a href='https://github.com/tharwin-carr'>tharwin-carr</a>
+			</p>
+			<p>
+				Part of <a href='http://todomvc.com'>TodoMVC</a>
+			</p>
+		</footer>
+	)
+}
---
+++ src/reducers/useTodos.js
@@ -0,0 +1,70 @@
+import { globalReducer } from 'react-hook-utils'
+
+import { guid } from '../utils'
+
+export const newTodo = (label) => ({
+	done: false,
+	id: guid(),
+	label: (label || '').trim(),
+	urgent: false,
+})
+
+export const reducer = {
+	// Delete a todo by id
+	deleteTodo: (state, id) => state.filter((i) => i.id !== id),
+
+	// Create a new item
+	addTodo: (state, label) => [newTodo(label), ...state],
+
+	// Set the done state of an item
+	setDone: (state, id, done) =>
+		state.map((i) =>
+			i.id === id
+				? {
+						...i,
+						done,
+				  }
+				: i
+		),
+
+	// Set the label of an item
+	setLabel: (state, id, label) =>
+		state.map((i) =>
+			i.id === id
+				? {
+						...i,
+						label,
+				  }
+				: i
+		),
+
+	// Toggle an item done
+	toggleDone: (state, id) =>
+		state.map((i) =>
+			i.id === id
+				? {
+						...i,
+						done: !i.done,
+				  }
+				: i
+		),
+
+	//Set the item as urgent
+	setUrgent: (state, id, urgent) =>
+		state.map((i) =>
+			i.id === id
+				? {
+						...i,
+						urgent: !i.urgent,
+				  }
+				: i
+		),
+}
+
+export default globalReducer(
+	// Load todos from local storage
+	JSON.parse(localStorage.getItem('todos') || '[]'),
+	reducer,
+	// On state change, persist to local storage
+	(todos) => localStorage.setItem('todos', JSON.stringify(todos))
+)
